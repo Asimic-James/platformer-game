b8c075cc0fc23b873fc37605de4bbca5a7daface
asimic-james <jayasimic@gmail.com>
2024-02-28 09:16:32 +0100

    new project

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ class Player {
     ctx.fillStyle = "#99c9ff";
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
-  
+
   update() {
     this.draw();
     this.position.x += this.velocity.x;
@@ -120,14 +120,25 @@ const animate = () => {
     }
   }
 
-
   platforms.forEach((platform) => {
     const collisionDetectionRules = [
       player.position.y + player.height <= platform.position.y,
       player.position.y + player.height + player.velocity.y >= platform.position.y,
       player.position.x >= platform.position.x - player.width / 2,
-      player.position.x <= platform.position.x + platform.width - player.width / 3,
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
     ];
+
+    if (collisionDetectionRules.every((rule) => rule)) {
+      player.velocity.y = 0;
+      return;
+    }
+
+
+    const platformDetectionRules = [
+      player.position.x >= platform.position.x - player.width / 2,
+    ];
+
   });
 
 }
